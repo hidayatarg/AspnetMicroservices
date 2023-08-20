98f813e56365b8e0881bbcb40f1e4ab2728dcaac
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-08-20 21:36:55 +0300

    [Catalog.API] Implemented Repository Pattern - Business Layer

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+    
+    Task<Product> GetProduct(string id);
+    
+    Task<IEnumerable<Product>> GetProductByName(string name);
+    
+    Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+    
+    Task CreateProduct(Product product);
+    
+    Task<bool> UpdateProduct(Product product);
+    
+    Task<bool> DeleteProduct(string id);
+}
