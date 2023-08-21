a4c36191a80855c7e13021235dc469af54765e76
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-08-21 09:07:38 +0300

    [Basket.API] Installed and Configured Redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Redis cache
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSetting:ConnectionString");
+    options.InstanceName = "Basket.API-";
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
