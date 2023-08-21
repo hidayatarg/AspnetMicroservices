8ddbddd5dc32d2274de79e79ce8e726b629e6b75
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-08-21 09:22:50 +0300

    [Basket.API] Implemented Repository Pattern

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart?> GetBasket(string userName);
+
+    Task<ShoppingCart?> UpdateBasket(ShoppingCart basket);
+
+    Task DeleteBasket(string userName);
+}
