e46eeeb8d1eabe20ec79bba3ef50fc9efae3a1be
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-08-22 22:39:18 +0300

    [Discount.API] Implemented Repository Pattron

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+using Discount.API.Entities;
+
+namespace Discount.API.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+    
+    Task<bool> CreateDiscount(Coupon coupon);
+    
+    Task<bool> UpdateDiscount(Coupon coupon);
+    
+    Task<bool> DeleteDiscount(string productName);
+}
