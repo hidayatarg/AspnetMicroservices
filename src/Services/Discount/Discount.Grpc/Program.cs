87bef9c198cea431b819e8f791dc6d3bc60967cb
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-01 11:54:24 +0300

    [Discount.Grpc] Added Respository Implementation from Discount.API

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // Seed data for postgresql
+    app.UseItToSeed();
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+    
+    Task<bool> CreateDiscount(Coupon coupon);
+    
+    Task<bool> UpdateDiscount(Coupon coupon);
+    
+    Task<bool> DeleteDiscount(string productName);
+}
