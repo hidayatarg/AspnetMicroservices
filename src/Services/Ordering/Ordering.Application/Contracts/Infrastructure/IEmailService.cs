8357266fe3ef4ca1ca52b7c88bb060b757b62ea4
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-07 18:00:56 +0300

    [Ordering.Application] Developed Application Layer Contracts

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
