736356bfffb36620bc16838a5acff0fe9e8ac33f
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-07 18:19:11 +0300

    [Ordering.Application] Added Application Layer Features -GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
