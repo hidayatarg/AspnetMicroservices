f6f0ec37bf1c4f8239afda4cb4bd9d416b0c67ad
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-07 22:04:41 +0300

    [Ordering.Application] Added Application Layer CommandHandlers-UpdateOrderCommandHandler

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{EmailAddress} must not exceed 50 characters.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} must be greater than zero.");
+        }
+    }
+}
