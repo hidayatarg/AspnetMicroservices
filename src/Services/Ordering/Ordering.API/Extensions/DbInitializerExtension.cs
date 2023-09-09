e3c59712a70a04787ecabfc57b708617844abf02
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-10 01:58:38 +0300

    [Ordering.API] Added DbInitializerExtension to Seed Data

---
+++ src/Services/Ordering/Ordering.API/Extensions/DbInitializerExtension.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.API.Extensions
+{
+    public static class DbInitializerExtension
+    {
+        public static IApplicationBuilder UseItToSeed(this IApplicationBuilder app)
+        {
+            ArgumentNullException.ThrowIfNull(app, nameof(app));
+
+            return HostExtensions.MigrateDatabase<OrderContext>(app);
+        }
+    }
+}
