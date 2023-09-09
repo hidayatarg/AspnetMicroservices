6440587c5bfbce15b466a924af7abe29970fbc42
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-09 12:00:43 +0300

    [Order.API] Added Registration for ApplicationServices and InfrastructureServices

--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using Ordering.Application.Features.Orders.Commands.DeleteOrder;
