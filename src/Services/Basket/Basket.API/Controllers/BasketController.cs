a78ea78080822e9a0330b54da480f415c1968d91
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-10 16:04:24 +0300

    [Basket.API] Completed the Publish BasketCheckout Queue Message Event

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -3,6 +3,7 @@ using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using EventBus.Messages.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -14,12 +15,14 @@ public class BasketController : ControllerBase
 {
     private readonly IBasketRepository _repository;
     private readonly DiscountGrpcService _discountGrpcService;
+    private readonly IPublishEndpoint _publishEndpoint;
     private readonly IMapper _mapper;
 
-    public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper)
+    public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper)
     {
         _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+        _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
@@ -70,13 +73,14 @@ public class BasketController : ControllerBase
         }
 
         // Create basketCheckoutEvent -- Set TotalPrice on basketCheckout eventMessage
-        // send checkout event to rabbitmq
         var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
-        //eventMessage.TotalPrice = basket.TotalPrice;
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        // send checkout event to rabbitmq
+        await _publishEndpoint.Publish(eventMessage);
 
         // remove the basket
         await _repository.DeleteBasket(basket.UserName);
-
 
         return Accepted();
     }
