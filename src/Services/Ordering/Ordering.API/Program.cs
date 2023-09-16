5fbeae0db5a3e6961d564dbd43e6faa44b410e0b
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-09-16 07:07:59 +0300

    [Ordering.API] Added Scope for BasketCheckoutConsumer

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -31,6 +31,7 @@ builder.Services.AddMassTransit(config =>
     });
 });
 builder.Services.AddAutoMapper(typeof(OrderingProfile));
+builder.Services.AddScoped<BasketCheckoutConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
