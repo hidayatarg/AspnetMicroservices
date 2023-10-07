953b66387224443444379279ed3c7038f48d86fd
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-10-07 17:51:27 +0300

    [APIGateway] Created Project & Added Logging Configuration

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// logging configuration
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
