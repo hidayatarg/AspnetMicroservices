0b09cf0d09bc65365ade543de227953b0aebfef7
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-10-12 04:48:59 +0300

    [APIGateway] Caching in Ocelot Api Gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -13,7 +14,8 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+    .AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
