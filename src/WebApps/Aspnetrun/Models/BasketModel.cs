8e1013347c10e754ae26233fd96cd4c4a61b8bc2
Hidayat Arghandabi <hidayatarg@gmail.com>
2024-01-15 15:51:53 +0300

    [UI] Added Models for OcelotGW

---
+++ src/WebApps/Aspnetrun/Models/BasketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aspnetrun.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemModel> Items { get; set; } = new List<BasketItemModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ src/WebApps/Aspnetrun/Models/CatalogModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Aspnetrun.Models
+{
+    /// <summary>
+    /// It is the DTO of the Consumed for Ocelot Gateway
+    /// </summary>
+    public class CatalogModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
