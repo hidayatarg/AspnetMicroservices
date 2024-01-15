9df92a5f027f2ba5edb028665ee3f5a7b3aa6eed
Hidayat Arghandabi <hidayatarg@gmail.com>
2024-01-15 17:11:24 +0300

    [UI] Updated Order and Cart Pages

--- src/WebApps/Aspnetrun/Pages/Order.cshtml.cs
+++ src/WebApps/Aspnetrun/Pages/Order.cshtml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Aspnetrun.Repositories;
+﻿using Aspnetrun.Models;
+using Aspnetrun.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,20 +7,20 @@ namespace Aspnetrun
 {
     public class OrderModel : PageModel
     {
-        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderService _orderService;
 
-        public OrderModel(IOrderRepository orderRepository)
+        public OrderModel(IOrderService orderService)
         {
-            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
         }
 
-        public IEnumerable<Entities.Order> Orders { get; set; } = new List<Entities.Order>();
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Orders = await _orderRepository.GetOrdersByUserName("test");
+            Orders = await _orderService.GetOrdersByUserName("swn");
 
             return Page();
-        }       
+        }
     }
 }
